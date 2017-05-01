a61bffcd23ab7e5e537f5d432a57eff77fae9469
Kelvin Kuiper <kelvin@dbk.nl>
2017-05-01 12:56:57 +0200

    added player

--- Game.js
+++ Game.js
@@ -16,10 +16,30 @@ BasicGame.Game.prototype = {
         this.backgroundLayer.resizeWorld();       
         
 
+        //create player
+        this.player = this.add.sprite(250, 0, 'dude');
+        this.physics.arcade.enable(this.player);
+        this.player.body.gravity.y = 600;
+        this.player.speed = 120;
+        this.player.animations.add('left', [0, 1, 2, 3], 10, true);
+        this.player.animations.add('right', [5, 6, 7, 8], 10, true);
+
     },
 
     update: function () {
- 
+        
+        if (this.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+            this.player.body.velocity.x = -this.player.speed;
+            this.player.animations.play('left');
+        } else if (this.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+            this.player.body.velocity.x = this.player.speed;
+            this.player.animations.play('right');
+        } else {
+            //idle
+            this.player.body.velocity.x = 0;
+            this.player.animations.stop();
+            this.player.frame = 4;
+        }
         
     },
 
--- Preloader.js
+++ Preloader.js
@@ -8,6 +8,8 @@ BasicGame.Preloader.prototype = {
     preload: function () {
         this.load.tilemap('mario', 'assets/tilemaps/super_mario.json', null, Phaser.Tilemap.TILED_JSON);
         this.load.image('tiles', 'assets/tilemaps/super_mario.png');
+
+        this.load.spritesheet('dude', 'assets/dude.png', 32, 48);
     },
 
     create: function () {
